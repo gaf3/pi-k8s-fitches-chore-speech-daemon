909da0f6517860e09305169824965d5a69a446d0
Gaffer Fitch <gaffer.fitch@gmail.com>
2018-11-26 04:51:26 +0000

    Initial code

---
+++ lib/service.py
@@ -0,0 +1,45 @@
+"""
+Main module for daemon
+"""
+
+import os
+import time
+import traceback
+
+import pi_k8s_fitches.chore_redis
+
+class Daemon(object):
+    """
+    Main class for daemon
+    """
+
+    def __init__(self):
+
+        self.chore_redis = pi_k8s_fitches.chore_redis.ChoreRedis(
+            host=os.environ['REDIS_HOST'],
+            port=int(os.environ['REDIS_PORT']),
+            channel=os.environ['REDIS_CHANNEL']
+        )
+        self.sleep = int(os.environ['SLEEP'])
+
+    def process(self):
+        """
+        Processes active chores for remindeds
+        """
+
+        for chore in self.chore_redis.list():
+            if "completed" not in chore:
+                self.chore_redis.remind(chore)
+    
+    def run(self):
+        """
+        Runs the daemon
+        """
+
+        while True:
+            try:
+                self.process()
+                time.sleep(self.sleep)
+            except Exception as exception:
+                print(str(exception))
+                print(traceback.format_exc())
---
+++ test/test_service.py
@@ -0,0 +1,103 @@
+import unittest
+import unittest.mock
+
+import os
+import json
+
+import service
+
+class MockChoreRedis(object):
+
+    def __init__(self, host, port, channel):
+
+        self.host = host
+        self.port = port
+        self.channel = channel
+
+        self.chores = []
+        self.reminded = []
+
+    def list(self):
+
+        return self.chores
+
+    def remind(self, chore):
+
+        self.reminded.append(chore)
+
+class TestService(unittest.TestCase):
+
+    @unittest.mock.patch.dict(os.environ, {
+        "REDIS_HOST": "data.com",
+        "REDIS_PORT": "667",
+        "REDIS_CHANNEL": "stuff",
+        "SLEEP": "7"
+    })
+    @unittest.mock.patch("pi_k8s_fitches.chore_redis.ChoreRedis", MockChoreRedis)
+    def setUp(self):
+
+        self.daemon = service.Daemon()
+
+    def test___init___(self):
+
+        self.assertEqual(self.daemon.chore_redis.host, "data.com")
+        self.assertEqual(self.daemon.chore_redis.port, 667)
+        self.assertEqual(self.daemon.chore_redis.channel, "stuff")
+        self.assertEqual(self.daemon.sleep, 7)
+
+    def test_process(self):
+
+        self.daemon.chore_redis.chores = [
+            {
+                "text": "nope",
+                "completed": 0
+            },
+            {
+                "text": "yep"
+            }
+        ]
+
+        self.daemon.process()
+
+        self.assertEqual(self.daemon.chore_redis.reminded, [
+            {
+                "text": "yep"
+            }
+        ])
+
+    @unittest.mock.patch("service.time.sleep")
+    @unittest.mock.patch("traceback.format_exc")
+    @unittest.mock.patch('builtins.print')
+    def test_run(self, mock_print, mock_traceback, mock_sleep):
+
+        self.daemon.chore_redis.chores = [
+            {
+                "text": "nope",
+                "completed": 0
+            },
+            {
+                "text": "yep"
+            }
+        ]
+
+        mock_sleep.side_effect = [None, Exception("whoops"), Exception("whoops")]
+        mock_traceback.side_effect = ["spirograph", Exception("doh")]
+
+        self.assertRaisesRegex(Exception, "doh", self.daemon.run)
+
+        self.assertEqual(self.daemon.chore_redis.reminded, [
+            {
+                "text": "yep"
+            },
+            {
+                "text": "yep"
+            },
+            {
+                "text": "yep"
+            }
+        ])
+        mock_print.assert_has_calls([
+            unittest.mock.call("whoops"),
+            unittest.mock.call("spirograph"),
+            unittest.mock.call("whoops")
+        ])
